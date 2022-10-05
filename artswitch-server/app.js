1b20f29f3d8fb76b69cbeef88bbf81bdb2426e09
KayodeOusi <kayodeolusi@gmail.com>
2022-10-05 14:23:01 +0100

    installed packages

---
+++ artswitch-server/app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
