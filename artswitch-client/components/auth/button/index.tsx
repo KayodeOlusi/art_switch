ada224e6474851802994a2abfad8914f4a8a00ff
KayodeOusi <kayodeolusi@gmail.com>
2022-10-10 19:59:14 +0100

    login page

--- artswitch-client/components/auth/button/index.tsx
+++ artswitch-client/components/auth/button/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Button = (props: Props) => {
-  return <button>Button</button>;
+  return (
+    <button className="bg-black w-80 px-4 py-3 rounded-md text-white mb-6">
+      Sign In
+    </button>
+  );
 };
 
 export default Button;
--- artswitch-client/pages/auth/login/index.tsx
+++ artswitch-client/pages/auth/login/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import LoginImage from "../../../public/assets/bg.png";
 import Button from "../../../components/auth/button";
 
 type Props = {};
@@ -7,29 +8,40 @@ type Props = {};
 const Login: NextPage = (props: Props) => {
   return (
     <div className="grid grid-cols-2 h-screen font-Inter">
-      <section className="form-section block p-4">
+      <section className="form-section block p-4 col-span-2 lg:col-span-1">
         <div className="logo font-semibold">ArtSwitch</div>
-        <div className="login-form">
-          <p>Welcome Back</p>
+        <div className="login-form h-5/6 flex flex-col items-center justify-center">
+          <p className="font-semibold text-4xl mb-4">Welcome Back</p>
           <form className="flex flex-col" onSubmit={() => {}}>
-            <input type="text" placeholder="Enter your email" />
-            <input type="password" placeholder="Password" />
-            <section>
-              <input type="checkbox" id="remember-me" />
-              <label htmlFor="remember-me">Remember Me</label>
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="border-2 border-gray-800 w-80 px-4 py-3 text-black rounded-md mb-6"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="border-2 border-gray-800 w-80 px-4 py-3 text-black rounded-md mb-6"
+            />
+            <section className="flex items-center mb-4">
+              <input
+                type="checkbox"
+                id="remember-me"
+                className="mr-2 cursor-pointer"
+              />
+              <label htmlFor="remember-me" className="text-sm font-semibold">
+                Remember Me
+              </label>
             </section>
             <Button />
           </form>
-          <p>Don't have an account? Sign Up</p>
+          <p className="text-sm font-semibold">
+            Don't have an account?{" "}
+            <span className="underline cursor-pointer">Sign Up</span>
+          </p>
         </div>
       </section>
-      <section className="image-section block">
-        <Image src={""} alt="ArtSwitch" />
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti omnis
-        quidem nisi ex architecto temporibus veritatis voluptatibus laborum
-        odit, laboriosam tempora ut incidunt adipisci quod minima! Blanditiis
-        saepe dolores ipsa!
-      </section>
+      <section className="hidden lg:block bg-[url('/assets/images/bg.png')] bg-no-repeat bg-center object-cover w-full h-full" />
     </div>
   );
 };
