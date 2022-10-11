b215366e51a9cba76dae967173ec046cea269270
KayodeOusi <kayodeolusi@gmail.com>
2022-10-11 15:02:04 +0100

    designed login page

--- artswitch-client/pages/auth/signup/index.tsx
+++ artswitch-client/pages/auth/signup/index.tsx
@@ -5,8 +5,17 @@ const SignUp: NextPage = () => {
   return (
     <div className="grid grid-cols-2 h-screen font-Inter">
       <section className="form-section block p-4 col-span-2 lg:col-span-1">
-        <div className="logo font-semibold">ArtSwitch</div>
-        <div className="login-form h-5/6 flex flex-col items-center justify-center">
+        <div className="logo font-semibold mb-4 px-4 lg:px-0 lg:p-4">
+          ArtSwitch
+        </div>
+        <section className="block relative px-4 lg:hidden">
+          <img
+            src={"/assets/svgs/bg-mobile.svg"}
+            alt=""
+            className="object-contain w-24 h-24"
+          />
+        </section>
+        <div className="login-form h-2/3 flex flex-col items-center justify-center lg:h-5/6">
           <p className="font-semibold text-4xl mb-4">Join the party</p>
           <SignupForm />
           <p className="text-sm font-semibold">
@@ -15,10 +24,13 @@ const SignUp: NextPage = () => {
           </p>
         </div>
       </section>
-      <section
-        className="hidden lg:block bg-[url('/assets/images/bg.png')]
-        bg-no-repeat bg-center object-cover w-full h-full"
-      />
+      <section className="hidden lg:block relative">
+        <img
+          src={"/assets/svgs/bg-desktop.svg"}
+          alt=""
+          className="w-full h-screen object-cover"
+        />
+      </section>
     </div>
   );
 };
