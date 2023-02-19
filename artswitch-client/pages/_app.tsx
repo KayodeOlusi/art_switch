d874085af0d64b8b63af5041f05f451e35702465
KayodeOlusi <kayodeolusi@gmail.com>
2023-02-19 15:54:42 +0100

    installed new project with jest

--- artswitch-client/pages/_app.tsx
+++ artswitch-client/pages/_app.tsx
@@ -1,14 +1,6 @@
-import "../styles/globals.css";
-import { Toaster } from "react-hot-toast";
-import type { AppProps } from "next/app";
+import '@/styles/global.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Toaster />
-    </>
-  );
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
