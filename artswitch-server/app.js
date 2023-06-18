324a963b86065edc7bbab79c796e3b6a10574f8b
KayodeOlusi <kayodeolusi@gmail.com>
2023-06-18 08:29:05 +0100

    routes api

--- artswitch-server/app.js
+++ artswitch-server/app.js
@@ -1,22 +1,25 @@
 require("dotenv").config();
+
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const auth_routes = require("./routes/auth");
+const posts_routes = require("./routes/posts");
 const connectDB = require("./config/db/dbConn");
 
 connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Terminal
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
 // Routes
 app.use("/api/auth", auth_routes);
+app.use("/api/posts");
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
---
+++ artswitch-server/routes/posts/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/all", () => {});
+router.get("/:id", () => {});
+
+module.exports = router;
