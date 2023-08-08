ff6c76ff0894f8082c5d4705358b3aaf97fd3eec
KayodeOlusi <kayodeolusi@gmail.com>
2023-08-08 23:43:34 +0100

    ft: user action

--- artswitch-server/controllers/user/follow.js
+++ artswitch-server/controllers/user/follow.js
@@ -2,37 +2,52 @@ const asyncHandler = require("express-async-handler");
 const Follow = require("../../models/follow");
 const { Types, isValidObjectId } = require("mongoose");
 
-const followUser = asyncHandler(async (req, res) => {
+const followProcess = asyncHandler(async (req, res) => {
   const user_id = req.user._id;
-  const { follow_id } = req.body;
+  const { follow_id, action } = req.body;
 
-  if (!follow_id || !isValidObjectId(follow_id))
-    return res.status(400).send("No follow_id provided");
+  if (!follow_id || !isValidObjectId(follow_id) || !action)
+    return res.status(400).send("Validation failed");
 
   let bulkFollowProcess = Follow.collection.initializeUnorderedBulkOp();
-
   const updateUserFollowing = bulkFollowProcess.find({
     user: Types.ObjectId(user_id),
   });
-  updateUserFollowing.following.set(follow_id, Types.ObjectId(follow_id));
-
   const updateUserFollowers = bulkFollowProcess.find({
     user: Types.ObjectId(follow_id),
   });
-  updateUserFollowers.followers.set(user_id, Types.ObjectId(user_id));
 
-  await Promise.all([updateUserFollowing.save(), updateUserFollowers.save()]);
+  switch (action) {
+    case "follow":
+      updateUserFollowing.following.set(follow_id, Types.ObjectId(follow_id));
+      updateUserFollowers.followers.set(user_id, Types.ObjectId(user_id));
+      await Promise.all([
+        updateUserFollowing.save(),
+        updateUserFollowers.save(),
+      ]);
+      break;
+    case "un-follow":
+      updateUserFollowers.followers.delete(user_id);
+      updateUserFollowing.following.delete(follow_id);
+      await Promise.all([
+        updateUserFollowing.save(),
+        updateUserFollowers.save(),
+      ]);
+      break;
+    default:
+      break;
+  }
 
   bulkFollowProcess.execute((err, result) => {
     if (err) return res.status(400).send(err.message);
 
     console.log(result);
     return res.status(200).json({
-      message: "Followed successfully",
+      message: "Action completed successfully",
     });
   });
 });
 
 module.exports = {
-  followUser,
+  followProcess,
 };
--- artswitch-server/routes/user/index.js
+++ artswitch-server/routes/user/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { followProcess } = require("../../controllers/user/follow");
 const { searchForUser, getUserDetails } = require("../../controllers/user");
 
 router.get("/", searchForUser);
 router.route("/profile").get(getUserDetails);
-router.post("/user/follow");
+router.post("/user/action", followProcess);
 
 module.exports = router;
