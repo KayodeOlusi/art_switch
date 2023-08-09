67cb62b1ab8c576c24b733a5c8694ce37975dfbe
KayodeOlusi <kayodeolusi@gmail.com>
2023-08-09 21:13:38 +0100

    follow process

--- artswitch-server/controllers/user/follow.js
+++ artswitch-server/controllers/user/follow.js
@@ -1,6 +1,40 @@
-const asyncHandler = require("express-async-handler");
 const Follow = require("../../models/follow");
+const asyncHandler = require("express-async-handler");
 const { Types, isValidObjectId } = require("mongoose");
+
+// @access  Private
+const performOperation = async (action, follow_id, user_id) => {
+  try {
+    await Follow.bulkWrite([
+      {
+        updateOne: {
+          filter: { user: Types.ObjectId(user_id) },
+          update: {
+            [action === "follow" ? "$set" : "$unset"]: {
+              [`following.${follow_id}`]: Types.ObjectId(follow_id),
+            },
+          },
+          upsert: true,
+        },
+      },
+      {
+        updateOne: {
+          filter: { user: Types.ObjectId(follow_id) },
+          update: {
+            [action === "follow" ? "$set" : "$unset"]: {
+              [`followers.${user_id}`]: Types.ObjectId(user_id),
+            },
+          },
+          upsert: true,
+        },
+      },
+    ]);
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 
 const followProcess = asyncHandler(async (req, res) => {
   const user_id = req.user._id;
@@ -9,45 +43,10 @@ const followProcess = asyncHandler(async (req, res) => {
   if (!follow_id || !isValidObjectId(follow_id) || !action)
     return res.status(400).send("Validation failed");
 
-  let bulkFollowProcess = Follow.collection.initializeUnorderedBulkOp();
-  const updateUserFollowing = bulkFollowProcess.find({
-    user: Types.ObjectId(user_id),
-  });
-  const updateUserFollowers = bulkFollowProcess.find({
-    user: Types.ObjectId(follow_id),
-  });
+  const result = await performOperation(action, follow_id, user_id);
+  if (!result) return res.status(500).json({ message: "Something went wrong" });
 
-  // TODO: Fix bulkFollowProcess to add to the map instead of replacing it
-
-  switch (action) {
-    case "follow":
-      updateUserFollowing.following.set(follow_id, Types.ObjectId(follow_id));
-      updateUserFollowers.followers.set(user_id, Types.ObjectId(user_id));
-      await Promise.all([
-        updateUserFollowing.save(),
-        updateUserFollowers.save(),
-      ]);
-      break;
-    case "un-follow":
-      updateUserFollowers.followers.delete(user_id);
-      updateUserFollowing.following.delete(follow_id);
-      await Promise.all([
-        updateUserFollowing.save(),
-        updateUserFollowers.save(),
-      ]);
-      break;
-    default:
-      break;
-  }
-
-  bulkFollowProcess.execute((err, result) => {
-    if (err) return res.status(400).send(err.message);
-
-    console.log(result);
-    return res.status(200).json({
-      message: "Action completed successfully",
-    });
-  });
+  return res.status(200).json({ message: "Success" });
 });
 
 module.exports = {
