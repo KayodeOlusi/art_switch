faa24094972cdbbd37b3ca80777c3c0ff9b8e6a9
KayodeOlusi <kayodeolusi@gmail.com>
2023-11-12 13:51:16 +0100

    ...

--- artswitch-client/utils/hooks/suggestions/useSuggestions.tsx
+++ artswitch-client/utils/hooks/suggestions/useSuggestions.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { TResponseBody } from "utils/services/typings/posts";
+import { TSuggestion } from "utils/services/typings/suggestions";
 import { getSuggestionsForUser } from "utils/services/user";
 
 export const useGetSuggestions = () => {
